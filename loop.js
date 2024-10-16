ae3893d2945ed5ea41ac7e15567e1ba60751b09f
ramjanmia110 <mdramjanmia112@gmail.com>
2024-10-16 21:09:40 +0600

    loop done

---
+++ loop.js
@@ -0,0 +1,65 @@
+// for loop start here
+
+let Namta1 = prompt("Enter A Number For Namta1");
+let Namta2 =prompt("Enter A Number For Namta2");
+let Namta3 =prompt("Enter A Number For Namta3");
+
+console.log("Use For Loop:")
+
+for (let i = 1; i <= 10; i++) {
+    console.log(Namta1 + " X " + i + " = " + (Namta1 * i) + "|" + 
+                Namta2 + " X " + i + " = " + (Namta2 * i) + "|" + 
+                Namta3 + " X " + i + " = " + (Namta3 * i) );
+}
+
+
+
+
+
+
+
+// while loop start here
+
+let Namta4 =prompt("Enter A Number For Namta4 ");
+let Namta5 =prompt("Enter A Number For Namta5 ");
+let Namta6 =prompt("Enter A Number For Namta6 ");
+
+console.log("While Loop Use:")
+let x=1;
+
+while(x<=10){
+    console.log(Namta1 + " X " + x + " = " + (Namta1 * x) + "|" + 
+    Namta2 + " X " + x + " = " + (Namta2 * x) + "|" + 
+    Namta3 + " X " + x + " = " + (Namta3 * x) );
+    x++;
+}
+
+
+// do while loop start here
+
+let Namta7 =prompt("Enter A Number For Namta7 ");
+let Namta8 =prompt("Enter A Number For Namta8 ");
+let Namta9 =prompt("Enter A Number For Namta9 ");
+
+console.log("Use Do While Loop :")
+
+let y =1;
+
+do{
+    console.log(Namta7 + " X " + y + " = " + (Namta7 * y) + "|" + 
+    Namta8 + " X " + y + " = " + (Namta8 * y) + "|" + 
+    Namta9 + " X " + y + " = " + (Namta9 * y) );
+    y++;
+}while(y<=10)
+
+
+
+
+
+
+
+
+
+
+
+
